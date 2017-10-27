db29dcd52f3e2f7df15a908e98262a4dd88a35ce
OlgaGulenko <olga.gulenko@hotmail.com>
2017-10-27 14:51:32 +0300

    createtest

---
+++ snakematrix.js
@@ -0,0 +1,23 @@
+
+var matrix = [];
+var rows = 4, cols = 6;
+var m = 1;
+var left = 1;
+
+for(var i=0,curr; i<rows; i++ ){
+    curr = matrix[i] = [];
+    for( var b=0; b<cols; b++ ){
+        curr[ left ? "push":"unshift" ]( m++ );
+    }
+    left = left == false;
+}
+
+var s = '<div class="matrix"><table width="600px" height="300px">';
+for( var i=0;i<matrix.length;i++ ){
+    s+= "<tr>";
+    for( var b=0;b<matrix[i].length;b++ )
+        s+= "<td>"+matrix[i][b]+"</td>";
+    s+="</tr>"
+}
+s+="</table></div>";
+document.body.innerHTML = s;
