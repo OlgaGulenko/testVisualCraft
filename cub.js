b0f1f9195c0174bb67ac91ec1f7049b51c1442b3
OlgaGulenko <olga.gulenko@hotmail.com>
2017-10-29 00:29:39 +0300

    test tasks

--- cub.js
+++ cub.js
@@ -1,94 +1,35 @@
 function draw() {
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
-    var a=10;
+    var a=1;
     var b=1;
-    // var c=100;
-    // var k=10;
-    // var i=1;
+    var isUnfold = true;
     var ctx = canvas.getContext('2d');
     ctx.strokeRect(10,10,500,500);
-    // console.log(ctx);
-    delay = 100,
+    delay = 5,
     i = 0,
     startTimer = function(q){
-      // console.log(ctx);
-      var qw=499;
-      var tt=249.5;
-      // if ((q > 0 && ))
-      if(i<qw && a<tt && b<tt){
+      var qw=500;
+      var tt=500;
+
+      if (a == 1 && b == 1){
+        isUnfold = true;
+      }else if (a == tt || b == tt){
+      	isUnfold = false;
+      }
+
+      if(isUnfold){
         a++;
         b++;
-        // c++;
-        // i++;
-        q++;
       } else{
-        clearInterval(timer);
-
-        ctx.clearRect (10, 10, 500, 500)
+        	a--;
+        	b--;
       }
-      i++;
-      console.log(i);
     }
     timer = setInterval(function (){
-
-      startTimer(ctx.fillRect(a,a,b,b),
-                )
-    }, delay)
+      ctx.clearRect(0,0,520,520);
+      startTimer(ctx.fillRect(10,10,a,b));
+     },
+    delay)
   }
 }
-
-// var a=ctx.fillRect(0,0,0,0);
-// for (a = 0; a<500; a++){
-//   return(setTimeout(ctx.fillRect(10,10,10,10), 10000))}
-//  var a=ctx.fillRect(10,10,10,10);
-//  for (a = 0; a<500; a++){
-//    setInterval(function() {
-//
-//    }, 10000);
-// }
-// function fon() {
-//   var i = ctx.fillRect(10,10,10,10);
-//   var timerId = setInterval(function() {
-//     console.log(i);
-//     if (i == 500) clearInterval(timerId);
-//     else {
-//       i++;
-//     }
-//   }, 1000);
-//   return i;
-// }
-
-//  for (a = 0; a<500; a++){
-//   setInterval(ctx.fillRect(10,10,10,10),10000)
-
-  // var timerId = setInterval(function() {
-  //   var a=ctx.fillRect(0,0,0,0)
-  //   for (a = 0; a<500; a++){
-  //     ctx.fillRect(10,10,10,10)
-  //   }
-  // }, 20000);
-  // //
-  // // через 5 сек остановить повторы
-  // setTimeout(function() {
-  //   clearInterval(timerId);
-  //
-  // }, 5000);
-
-  // }
-  // do {
-  //   setTimeout(ctx.fillRect(10,10,10,10), 1000)
-  // } while (a = 0; a<500; a++);
-
-// requestAnimationFrame(ctx.fillRect(10,10,10,10))
-
-    // setInterval(function(){
-    //   var a=ctx.fillRect(0,0,0,0);
-    //     if(a = 0; a<500; a++){
-    //         ctx.fillRect(10,10,10,10);
-    //         leftOrRight = "right";
-    //     }else{
-    //         ctx.clearRect(10,10,10,10);
-    //         leftOrRight = "left";
-    //     }
-    // }, 1000);
--- snakematrix.js
+++ snakematrix.js
@@ -1,15 +1,16 @@
-
 var matrix = [];
 var rows = 4, cols = 6;
 var m = 1;
-var left = 1;
+// var left = 1;
+var isLeft = true;
 
-for(var i=0,curr; i<rows; i++ ){
-    curr = matrix[i] = [];
+for(var i=0; i<rows; i++ ){
+    var line = [];
     for( var b=0; b<cols; b++ ){
-        curr[ left ? "push":"unshift" ]( m++ );
+        isLeft ? line.push(m++) : line.unshift(m++);
     }
-    left = left == false;
+    matrix[i] = line;
+    isLeft = !isLeft;
 }
 
 var s = '<div class="matrix"><table width="600px" height="300px">';
@@ -21,3 +22,28 @@ for( var i=0;i<matrix.length;i++ ){
 }
 s+="</table></div>";
 document.body.innerHTML = s;
+
+//=================================================
+// var matrix = [];
+// var rows = 4, cols = 6;
+// var m = 1;
+// var left = 1;
+//
+// for(var i=0,curr; i<rows; i++ ){
+//     curr = matrix[i] = [];
+//
+//     for( var b=0; b<cols; b++ ){
+//         curr[ left ? "push":"unshift" ]( m++ );
+//     }
+//     left = left == false;
+// }
+//
+// var s = '<div class="matrix"><table width="600px" height="300px">';
+// for( var i=0;i<matrix.length;i++ ){
+//     s+= "<tr>";
+//     for( var b=0;b<matrix[i].length;b++ )
+//         s+= "<td>"+matrix[i][b]+"</td>";
+//     s+="</tr>"
+// }
+// s+="</table></div>";
+// document.body.innerHTML = s;
